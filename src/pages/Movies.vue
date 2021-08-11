d038ac6d58a8a49b92fe3b44598800c6495ea367
Yohan Arias <elyohan14@gmail.com>
2021-08-11 16:59:36 -0400

    Modal Agregar peli

--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -7,10 +7,47 @@
       row-key="name">
      <template v-slot:top>
        <div class="row">
-          <q-btn color="primary" icon="add" label="Añadir" rounded/>
+          <q-btn color="primary" icon="add" label="Añadir" @click="modalAdd = true" rounded/>
        </div>
       </template>
      </q-table>
+     <q-dialog v-model="modalAdd">
+       <q-card class="q-pa-md" style="width: 420px">
+         <div class="col text-h6 text-bold ellipsis">
+            Añadir película
+          </div>
+          <div class="row">
+            <div class="col-md-12 q-pb-sm">
+              <label for="">Nombre</label>
+              <q-input outlined v-model="form.name" dense/>
+            </div>
+            <div class="col-md-12 q-pb-sm">
+              <label for="">Descripción</label>
+              <q-input type="textarea" outlined v-model="form.name" dense/>
+            </div>
+            <div class="col-md-3 q-pr-xs q-pb-sm">
+              <label for="">Calificación</label>
+              <q-input outlined v-model="form.name" dense/>
+            </div>
+            <div class="col-md-9 q-pl-xs">
+              <label for="">Categoría</label>
+              <q-input outlined v-model="form.name" dense/>
+            </div>
+            <div class="col-md-5 q-pr-xs">
+              <label for="">Carátula Url</label>
+              <q-input outlined v-model="form.name" dense/>
+            </div>
+            <div class="col-md-7 q-pl-xs">
+              <label for="">Portada Url</label>
+              <q-input outlined v-model="form.name" dense/>
+            </div>
+          </div>
+          <q-card-actions align="right">
+            <q-btn flat label="Cancelar" color="primary" v-close-popup />
+            <q-btn flat label="Añadir" color="primary" v-close-popup />
+          </q-card-actions>
+       </q-card>
+     </q-dialog>
   </q-page>
 </template>
 
@@ -19,6 +56,8 @@ export default {
   name: 'PageIndex',
   data () {
     return {
+      modalAdd: false,
+      form: {},
       columns: [
         {
           name: 'name',
